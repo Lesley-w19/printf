b643c980df91d312c2dce5b2b7c508e58d8ee9d1
Lesley-w19 <lesley.kamamo@gmail.com>
2022-06-11 13:10:07 -0700

    printf

--- main.h
+++ main.h
@@ -1,9 +1,14 @@
-#ifndef PRINTF
-#define PRINTF
+#ifndef PRINTF_
+#define PRINTF_
+#include <stdarg.h>
+
+int print_percent(va_list ap __attribute__((unused)));
+
 typedef struct func
 {
 	char c;
 	void (*output_sth)(va_list);
 } choose_func;
 int _putchar(char c);
+
 #endif
